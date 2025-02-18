a679c1185cd58d8c26485208870854563d0d6431
teshawnwouter <145546485+teshawnwouter@users.noreply.github.com>
2025-02-18 09:45:13 +0100

    working on the wallRun

--- Assets/Scripts/BasicMovement.cs
+++ Assets/Scripts/BasicMovement.cs
@@ -1,4 +1,3 @@
-using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -11,18 +10,33 @@ public class BasicMovement : MonoBehaviour
     private Vector2 lookVector;
 
     private float walkSpeed = 5f;
-
     private float sprintSpeed = 25f;
+    private float wallRunSpeed = 12f;
     private bool isSprinting;
 
     private float jumpForce = 5f;
 
     private bool isGrounded;
+    private bool isOnWall;
     private float playerHeigt = 1.6f;
     [SerializeField] private LayerMask whatIsGround;
 
-
     float rotationspeed = 4f;
+
+    [Header("Wallrun")]
+    public LayerMask whatIsWall;
+    public float wallForce;
+    public float maxWallRunTime;
+    private float wallRunTime;
+
+    [Header("detections")]
+    public float wallCheckDetection;
+    public float minwallJump;
+    public RaycastHit wallLeftHit;
+    public RaycastHit wallRightHit;
+    public bool onWallRight;
+    public bool onWallLeft;
+
 
     private void Awake()
     {
@@ -44,19 +58,22 @@ public class BasicMovement : MonoBehaviour
             }
         }
 
-        if(rb.linearVelocity != Vector3.zero)
+        if ((onWallLeft || onWallRight) && AboveGround())
         {
-            float targetRotation = Mathf.Atan2(moveVector.x,moveVector.y) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
-            Quaternion turning =  Quaternion.Euler(0f,targetRotation,0f);
+
+        }
+
+        if (rb.linearVelocity != Vector3.zero)
+        {
+            float targetRotation = Mathf.Atan2(moveVector.x, moveVector.y) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
+            Quaternion turning = Quaternion.Euler(0f, targetRotation, 0f);
             transform.rotation = Quaternion.Lerp(transform.rotation, turning, Time.deltaTime * rotationspeed);
         }
-    }
 
-    private void LateUpdate()
-    {
-        
+        if (isOnWall)
+            WallRunMovement();
     }
-
+    #region Inputs
     public void OnWalk(InputAction.CallbackContext context)
     {
         moveVector = context.ReadValue<Vector2>();
@@ -86,6 +103,44 @@ public class BasicMovement : MonoBehaviour
     public void OnLook(InputAction.CallbackContext context)
     {
         lookVector = context.ReadValue<Vector2>();
+    }
+    #endregion
+
+    private void CheckForWall()
+    {
+        onWallRight = Physics.Raycast(transform.position, transform.right, out wallRightHit, wallCheckDetection, whatIsWall);
+        onWallLeft = Physics.Raycast(transform.position, transform.right, out wallLeftHit, wallCheckDetection, whatIsWall);
+    }
+
+    private bool AboveGround()
+    {
+        return !Physics.Raycast(transform.position, Vector3.down, minwallJump, whatIsGround);
+    }
+
+    private void StartWallRun()
+    {
+        isOnWall = true;
+    }
+
+    private void WallRunMovement()
+    {
+        rb.useGravity = false;
+
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
+
+        Vector3 wallNormal = onWallRight ? wallRightHit.normal : wallLeftHit.normal;
+
+        Vector3 forwardMovementOnWal = Vector3.Cross(wallNormal, transform.up);
+
+        if ((transform.forward - forwardMovementOnWal).magnitude > (transform.forward - -forwardMovementOnWal).magnitude)
+            forwardMovementOnWal = -forwardMovementOnWal;
+
+            rb.AddForce(forwardMovementOnWal * wallForce, ForceMode.Force);
+    }
+
+    private void StopWallRun()
+    {
+        isOnWall = false;
     }
 
 }
