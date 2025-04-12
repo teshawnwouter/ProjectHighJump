81b12b24f7644df39a449823b11612d3a0a240e8
teshawnwouter <145546485+teshawnwouter@users.noreply.github.com>
2025-04-12 23:02:17 +0200

    Revert "finished"
    This reverts commit db90040114ffd4e336287d2422b04354aa7789e5.

--- Assets/Scripts/Systems/ButtonManager.cs
+++ Assets/Scripts/Systems/ButtonManager.cs
@@ -4,8 +4,6 @@ using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
 {
-    public GameObject credits;
-    public GameObject main;
     public void MainMenu()
     {
         SceneManager.LoadScene("MainMenu");
@@ -19,17 +17,5 @@ public class ButtonManager : MonoBehaviour
     public void StartGame()
     {
         SceneManager.LoadScene("MainGame");
-    }
-
-    public void Credits()
-    {
-        credits.SetActive(true);
-        main.SetActive(false);
-    }
-
-    public void Back()
-    {
-        main.SetActive(true);
-        credits.SetActive(false);
     }
 }
